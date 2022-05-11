8d2a50f1c8b6026eb5b93fd507ae6d0175ff5e54
kwenealete <kwenait@yahoo.ca>
2022-05-11 08:39:00 +0200

    Pagination with dynamic routing functionality, adjusting query for pagination values and custom type policies and control over the apollo cache

---
+++ frontend/pages/paginationField.js
@@ -0,0 +1,50 @@
+import { PAGINATION_QUERY } from "../components/Pagination";
+
+export default function paginationField() {
+    return {
+        keyArgs: false, //imforms apollo we will handle everything
+        read(existing = [], { args, cache }) {
+            const { skip, first } = args;
+
+            // Read the nuber of items on the page from the cache
+            const data = cache.readQuery({ query: PAGINATION_QUERY });
+            const count = data?._allProductsMeta?.count;
+            const page = skip / first + 1;
+            const pages = Math.ceil(count / first);
+            
+            // Check for existing items
+            const items = existing.slice(skip, skip + first).filter((x) => x);
+
+            // If there are items and not enough to satisfy our request, and we're on last page
+            // Then just send it
+            if (items.length && items.length !== first && page === pages) {
+                return items;
+            }
+            if (items.length !== first) {
+                // No items found, so we need to check the network
+                return false;
+            }
+
+            // If there are items, we'll just return from the cache and forget going to the network
+            if (items.length) {
+                console.log(`There are ${items.length} items in the cache! Sending them to apollo`);
+                return items;
+            }
+
+            return false; // Fallback to network
+        },
+        merge(existing, incoming, { args }) {
+            const { skip, first } = args;
+            // It runs when apollo clients comes back with our products from the network
+            console.log(`merging items from the network ${incoming.length}`);
+            const merged = existing ? existing.slice(0) : [];
+            for (let i = skip; i < skip + incoming.length; ++i) {
+                merged[i] = incoming[i - skip];
+            }
+
+            // We finally return the merged items from the cache
+            return merged;
+            
+        }
+    };
+}
--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/dist/client/router';
 import Products from "../components/Products";
+import Pagination from "../components/Pagination";
 
 export default function ProductsPage() {
-    return <div>
-        <Products />
-    </div>
+    const { query } = useRouter();
+    const page = parseInt(query.page);
+    return (
+        <div>
+            <Pagination page={page || 1} />
+            <Products page={page || 1} />
+            <Pagination page={page || 1} />
+        </div>
+    );
 }
