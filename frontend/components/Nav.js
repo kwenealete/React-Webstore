a2366bf212f0a8a5b23f01eaff762c219badf608
kwenealete <kwenait@yahoo.ca>
2022-05-17 06:06:47 +0200

    User regstration: querying the current user, creating a sign in component, a sign out component and our signup flow

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import NavStyles from './styles/NavStyles';
+import { useUser } from "./User";
+import SignOut from "./SignOut";
 
 export default function Nav() {
-    return <NavStyles>
+    const user = useUser();
+    return (
+     <NavStyles>
        <Link href="/products">Products</Link>
-       <Link href="/sell">Sell</Link>
-       <Link href="/order">Orders</Link>
-       <Link href="/account">Account</Link>
-    </NavStyles>
+       {user && (
+           <>
+            <Link href="/sell">Sell</Link>
+            <Link href="/order">Orders</Link>
+            <Link href="/account">Account</Link>
+            <SignOut />
+           </>
+       )}
+       {!user && (
+           <>
+            <Link href="/signin">Sign In</Link>
+           </>
+       )}
+     </NavStyles>)
 }
---
+++ frontend/components/SignUp.js
@@ -0,0 +1,87 @@
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Error from './ErrorMessage';
+
+const SIGNUP_MUTATION = gql`
+    mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+        createUser (data: {
+            email: $email, name: $name, password: $password
+        }) {
+            id
+            email
+            name
+        }
+    }
+`;
+
+export default function SignUp() {
+    const { inputs, handleChange, resetForm } = useForm({
+        email: '',
+        name: '',
+        password: ''
+    });
+
+    const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+        variables: inputs,
+       
+    });
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log(inputs);
+        const res = await signup().catch(console.error);
+        console.log(res);
+        resetForm();
+        
+    }
+   
+    return (
+        <Form method="POST" onSubmit={handleSubmit}>
+            <h2>Sign Up For An Account</h2>
+            <Error error={error} />
+            <fieldset>
+                {data?.createUser && <p>Signed up with {data.createUser.email}- please log in. </p>}
+                <label htmlFor="email">
+                    Email
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email address"
+                        autoComplete="email"
+                        value={inputs.email}
+                        onChange={handleChange}
+
+                    />
+                </label>
+                <label htmlFor="name">
+                    Your Name
+                    <input 
+                        type="text"
+                        name="name"
+                        placeholder="Enter your name"
+                        autoComplete="name"
+                        value={inputs.name}
+                        onChange={handleChange}
+
+                    />
+                </label>
+                <label htmlFor="password">
+                    Password
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password "
+                        autoComplete="password"
+                        value={inputs.password}
+                        onChange={handleChange}
+
+                    />
+                </label>
+                <button type="submit">Sign In!</button>
+            </fieldset>
+        </Form>
+    );
+    
+}
